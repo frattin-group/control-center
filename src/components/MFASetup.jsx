ae04a32ce1ff1d68b4728f95a0d30fab267f0d19
Mattia Rossi <rossimattia1989@gmail.com>
2025-12-04 17:44:03 +0100

    Fix: use user.verifyTOTP() for MFA verification

--- src/components/MFASetup.jsx
+++ src/components/MFASetup.jsx
@@ -32,7 +32,7 @@ export default function MFASetup({ onComplete }) {
 
         setIsVerifying(true);
         try {
-            await totp.attemptVerification({ code });
+            await user.verifyTOTP({ code });
             setStep('success');
             toast.success("Autenticazione a due fattori attivata!");
             setTimeout(() => {
