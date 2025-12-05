ba371bb7af76c6b142ea187e9b19dd7ce7273d7c
Mattia Rossi <rossimattia1989@gmail.com>
2025-12-05 08:29:04 +0100

    fix: restore Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,19 +9,3 @@ export default {
   },
   plugins: [],
 }
-
-module.exports = {
-  theme: {
-    extend: {
-      animation: {
-        'shimmer': 'shimmer 2s linear infinite',
-      },
-      keyframes: {
-        shimmer: {
-          '0%': { transform: 'translateX(-100%)' },
-          '100%': { transform: 'translateX(100%)' },
-        }
-      }
-    }
-  }
-}
